d32ff582b1f9af0350312e1272fb9f87304380cb
Vincent <568189127@qq.com>
2015-12-01 16:58:13 +0800

    the confilct has been resolved“

--- src/cn/edu/xmu/artworkauction/controller/RegisterLoginController.java
+++ src/cn/edu/xmu/artworkauction/controller/RegisterLoginController.java
@@ -58,32 +58,15 @@ public class RegisterLoginController {
 		}
 		else 
 			modelAndView =new ModelAndView(user.getUserType());
-		/*
-		if(user.getUserType().equals("artist")){
-			Artist artist=(Artist)user;
-			//modelAndView.addObject(artist);
-			request.getSession().setAttribute("artist", artist);
-			return modelAndView;
-		}
-		*/
-		//modelAndView.addObject(user);
 		request.getSession().setAttribute("user", user);
 		return modelAndView;
 	}
 	
-<<<<<<< HEAD
-	private Admin admin;
-=======
->>>>>>> c77741e062caf1ee0eae193487ee385b7aedde43
 	@RequestMapping ("/adminLogin")
 	public ModelAndView adminLogin(HttpServletRequest request) {
 		String adminName=request.getParameter("adminName");
 		String password=request.getParameter("password");
-<<<<<<< HEAD
-		ModelAndView modelAndView=new ModelAndView("Admin/admin_main");
-		admin=secureService.adminLogin(adminName, password);
-		modelAndView.addObject(admin);
-=======
+
 		Admin admin=secureService.adminLogin(adminName, password);
 		ModelAndView modelAndView ;
 		if(admin==null) {
@@ -91,25 +74,6 @@ public class RegisterLoginController {
 		}
 		else
 			modelAndView=new ModelAndView(admin.getAdminType());
-		/*
-		if(admin.getAdminType().equals("editor")) {
-			Editor editor=(Editor)admin;
-			request.getSession().setAttribute("editor", editor);
-		}
-		else if(admin.getAdminType().equals("chiefEditor")) {
-			ChiefEditor chiefEditor=(ChiefEditor)admin;
-			request.getSession().setAttribute("chiefEditor", chiefEditor);
-		}
-		else if(admin.getAdminType().equals("userEditor")) {
-			UserAdmin userAdmin =(UserAdmin)admin;
-			request.getSession().setAttribute("userAdmin",userAdmin);
-		}
-		else {
-			ServiceAdmin serviceAdmin=(ServiceAdmin)admin;
-			request.getSession().setAttribute("serviceAdmin", serviceAdmin);
-		}
-		*/
->>>>>>> c77741e062caf1ee0eae193487ee385b7aedde43
 		request.getSession().setAttribute("admin", admin);
 		return modelAndView;
 	}
