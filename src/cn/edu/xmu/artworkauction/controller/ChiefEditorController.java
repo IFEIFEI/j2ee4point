bacf58914ff0a6929f817dc4994624c48f4eb63c
dany <ifeifei@stu.xmu.edu.cn>
2015-12-13 00:43:56 +0800

    在chiefeditorcontroller里面做了一个简单的ajax测试，测试的页面是test.jsp 你们可以参考看看前台是怎么发送数据的

--- src/cn/edu/xmu/artworkauction/controller/ChiefEditorController.java
+++ src/cn/edu/xmu/artworkauction/controller/ChiefEditorController.java
@@ -1,7 +1,37 @@
 package cn.edu.xmu.artworkauction.controller;
-/**
- * @author 
- * */
-public class ChiefEditorController {
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @ChiefEditorController
+ * @author Dany ifeifei@stu.xmu.edu.cn
+ * */
+@Controller
+public class ChiefEditorController 
+{
+	@ResponseBody
+	@RequestMapping("/jstest")
+	public String test(HttpServletRequest request,HttpServletResponse response)
+	{
+		System.out.println("successed to receive the ajax json");
+		String title=request.getParameter("jstest");
+		JSONObject root=new JSONObject();
+		JSONArray jsonArray=new JSONArray();
+		root.put("title", "title");
+		root.put("article", "article");
+		root.put("type", "type");
+		System.out.println(title);
+		System.out.println(root);
+		return root.toString();
+	}
 }
